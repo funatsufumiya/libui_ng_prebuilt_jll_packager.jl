292c560f560f84b09c55904520cf06e344267ba0
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-05-04 16:04:52 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# libui_ng_prebuilt_jll_packager.jl
+
+- deploy.jl: Upload specified folder to gist (for artifacts)
+
+## Usage
+
+### Show help
+
+```bash
+$ julia --project=@. deploy.jl -h
+```
---
+++ deploy.jl
@@ -0,0 +1,22 @@
+using ArgParse
+
+function parse_commandline()
+    s = ArgParseSettings(description="upload folder to gist")
+
+    @add_arg_table s begin
+        "dir"
+            help = "deploy target directory"
+            required = true
+    end
+
+    return parse_args(s)
+end
+
+parsed_args = parse_commandline()
+dir::String = parsed_args["dir"]
+if !isdir(dir)
+    println("[Error] given dir not exist: $dir")
+    exit(1)
+end
+
+println("dir: $dir")
